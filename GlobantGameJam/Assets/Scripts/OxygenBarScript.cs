e5b11403eb1a4c0dd350360e48a80daf0bd45f32
judajaavAlt <132619919+judajaavAlt@users.noreply.github.com>
2025-01-26 12:18:07 -0500

    prefab oxygen

--- GlobantGameJam/Assets/Scripts/OxygenBarScript.cs
+++ GlobantGameJam/Assets/Scripts/OxygenBarScript.cs
@@ -12,9 +12,6 @@ public class OxygenBarScript : MonoBehaviour
     {
         if (player != null)
         {
-            Vector3 position = player.transform.position;
-            position.y += 0.25f;
-            transform.position = position;
             Vector3 scale = transform.localScale;
             scale.x = 0.5f * (player.GetComponent<PlayerScript>().Oxygen / 100);
             transform.localScale = scale;
