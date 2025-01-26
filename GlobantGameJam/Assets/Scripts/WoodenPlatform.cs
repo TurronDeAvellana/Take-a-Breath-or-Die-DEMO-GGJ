736d96e3cf97e28f70ab1747f056e838012ec36f
d1n0s4uri0 <juan.jose.paredes@correounivalle.edu.co>
2025-01-25 19:13:59 -0500

    platform v2

--- GlobantGameJam/Assets/Scripts/WoodenPlatform.cs
+++ GlobantGameJam/Assets/Scripts/WoodenPlatform.cs
@@ -7,10 +7,8 @@ public class WoodenPlatform : MonoBehaviour
 {
     [SerializeField] private float waitTime;
     [SerializeField] private float respawnTime;
-    public Collider2D playerCollider;
-    public Collider2D platformCollider;
     private Vector3 initialPosition;
-    private bool isPlayerBelow = false;
+  
 
     void Start()
     {
@@ -19,30 +17,14 @@ public class WoodenPlatform : MonoBehaviour
 
     private void Update()
     {
-        if (isPlayerBelow && Input.GetButtonDown("Jump"))
-        {
-            Physics2D.IgnoreCollision(playerCollider, platformCollider, true);
-        }
-        else if (!isPlayerBelow && playerCollider.bounds.min.y > platformCollider.bounds.max.y)
-        {
-            Physics2D.IgnoreCollision(playerCollider, platformCollider, false);
-        }
+       
     }
 
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.collider == platformCollider)
-        {
-
-            Physics2D.IgnoreCollision(playerCollider, platformCollider, false);
-        }
-    }
-
+   
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            isPlayerBelow = playerCollider.bounds.max.y < platformCollider.bounds.min.y;
 
             foreach (ContactPoint2D contact in collision.contacts)
             {
